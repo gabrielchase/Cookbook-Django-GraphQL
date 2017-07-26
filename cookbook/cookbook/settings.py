536cb1a905be1378feba10f0834905152fcac114
gabrielchase <gchase.patron@gmail.com>
2017-07-26 23:25:27 +0800

    Graphene settings and graphql url

--- cookbook/cookbook/settings.py
+++ cookbook/cookbook/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+    
     'ingredients'
 ]
 
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'cookbook.schema.schema'
+}
