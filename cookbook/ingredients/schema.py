fe8cc464e935782c9c829eb5e8f44319cde4c135
gabrielchase <gchase.patron@gmail.com>
2017-07-27 01:12:02 +0800

    Filter on both types

--- cookbook/ingredients/schema.py
+++ cookbook/ingredients/schema.py
@@ -1,6 +1,7 @@
 import graphene
 
 from graphene_django.types import DjangoObjectType
+from graphene_django.filter.fields import DjangoFilterConnectionField
 
 from ingredients.models import (
     Category,
@@ -10,27 +11,38 @@ from ingredients.models import (
 class CategoryType(DjangoObjectType):
     class Meta:
         model = Category
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith']
+        }
+        interfaces = (graphene.Node,)
 
 
 class IngredientType(DjangoObjectType):
     class Meta:
         model = Ingredient
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'notes': ['icontains'],
+            'category__name': ['exact']
+        }
+        interfaces = (graphene.Node,)
 
 
 class Query(graphene.AbstractType):
+    ingredient = graphene.Field(
+        IngredientType, 
+        id=graphene.ID(), 
+        name=graphene.String()
+    )
+
     category = graphene.Field(
         CategoryType,
         id=graphene.Int(),
         name=graphene.String()
     )
-    all_categories = graphene.List(CategoryType)
-
-    ingredient = graphene.Field(
-        IngredientType,
-        id=graphene.Int(),
-        name=graphene.String()
-    )
-    all_ingredients = graphene.List(IngredientType)
+    
+    all_categories = DjangoFilterConnectionField(CategoryType)
+    all_ingredients = DjangoFilterConnectionField(IngredientType)
 
     def resolve_category(self, args, context, info):
         id = args.get('id')
@@ -46,7 +58,6 @@ class Query(graphene.AbstractType):
 
     def resolve_all_categories(self, args, context, info):
         return Category.objects.all()
-
 
     def resolve_ingredient(self, args, context, info):
         id = args.get('id')
