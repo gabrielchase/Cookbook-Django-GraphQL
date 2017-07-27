e8255f26ba9f4a7dd3fb821cb5f3665dc99e9676
gabrielchase <gchase.patron@gmail.com>
2017-07-28 00:06:04 +0800

    Added mutations to project level schema and `csrf_excempt` in the graphql endpoint

--- cookbook/cookbook/schema.py
+++ cookbook/cookbook/schema.py
@@ -2,9 +2,11 @@ import graphene
 
 import ingredients.schema
 
+class Mutation(ingredients.schema.Mutation, graphene.ObjectType):
+    pass
 
 class Query(ingredients.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
